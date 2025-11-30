dac6db576de6236dc7b2b30efe4464bf7c0e2bd8
ChakriSriSaiPavan <145587697+ChakriSriSaiPavan@users.noreply.github.com>
2025-11-29 19:31:39 -0600

    Initial Commit

---
+++ src/main/java/com/demo/helloworld/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.demo.helloworld;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String hostname = java.net.InetAddress.getLocalHost().getHostName();
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello from " + hostname + "</h1>");
+    }
+}
